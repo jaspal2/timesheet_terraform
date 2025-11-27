e8ef02dd4de96e91265dcf28a293fe64a2a84ab2
Jaspal singh <jaspal.singh@monash.edu>
2025-11-27 14:55:16 +1100

    First terraform

--- main.tf
+++ main.tf
@@ -1,1 +1,28 @@
-hhh
+data "aws_ami" "ubuntu" {
+  most_recent      = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu-noble-24.04-amd64-server*"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+resource "aws_instance" "web" {
+  ami           = data.aws_ami.ubuntu.id
+  instance_type = "t3.micro"
+
+  tags = {
+    Name = "HelloWorld"
+  }
+}
