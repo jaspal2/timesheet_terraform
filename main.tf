22d1776e3f72629b834d710fd15a4f8cf7e64461
Jaspal singh <jaspal.singh@monash.edu>
2025-12-04 13:39:10 +1100

    Added SG and VPC

--- main.tf
+++ main.tf
@@ -43,28 +43,7 @@ module "vpc" {
 }
 
 module "vote_service_sg" {
-  source = "terraform-aws-modules/security-group/aws"
-
-  name        = "user-service"
-  description = "Security group for user-service with custom ports open within VPC, and PostgreSQL publicly open"
-  vpc_id      = module.vpc.vpc_id
-
-
-  ingress_cidr_blocks      = ["0.0.0.0/0"]
-  ingress_rules            = ["https-443-tcp", "ssh-tcp"]
-  ingress_with_cidr_blocks = [
-    {
-      from_port   = 8080
-      to_port     = 8090
-      protocol    = "tcp"
-      description = "User-service ports"
-      cidr_blocks = "10.10.0.0/16"
-    },
-    {
-      rule        = "postgresql-tcp"
-      cidr_blocks = "0.0.0.0/0"
-    },
-  ]
+  source = "./security-group"
 }
 resource "aws_instance" "web" {
   ami           = data.aws_ami.ubuntu.id
